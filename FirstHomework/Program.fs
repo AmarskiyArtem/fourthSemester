bdcbc29c9f2c13709c23bc823e9774c9ea690913
Artem <amarskiywork@gmail.com>
2024-02-22 22:43:13 +0300

    add factorial

--- FirstHomework/Program.fs
+++ FirstHomework/Program.fs
@@ -1,2 +1,9 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿let factorial n = 
+    if n < 0 then invalidArg "n""n can't be negative" else
+    let rec helper (acc:int) n =
+        if n = 0 then acc
+        else helper (acc * n) (n - 1)
+    helper 1 n
+
+
+printfn "%d" (factorial 10)
