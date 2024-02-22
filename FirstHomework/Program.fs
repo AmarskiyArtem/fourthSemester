abb9ac3f519f486c70bd78d9ba2c4c6d8729f889
Artem <amarskiywork@gmail.com>
2024-02-23 00:08:36 +0300

    add fibonacci

--- FirstHomework/Program.fs
+++ FirstHomework/Program.fs
@@ -1,9 +1,21 @@
 ﻿let factorial n = 
     if n < 0 then invalidArg "n""n can't be negative" else
-    let rec helper (acc:int) n =
+    let rec helper acc n =
         if n = 0 then acc
         else helper (acc * n) (n - 1)
     helper 1 n
 
+let fibonacci n =
+    if n < 0 then invalidArg "n""n can't be negative" else
+    let rec helper a b n =
+        if n = 0 then a
+        else helper b (a + b) (n - 1)
+    helper 0 1 n
 
-printfn "%d" (factorial 10)
+
+let rec x a =
+    printfn "%d" (fibonacci a)
+    x (a - 1)
+
+x 10
+
