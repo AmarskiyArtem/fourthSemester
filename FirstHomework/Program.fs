0f76a2ff575df96804c69e9e383fe0788b01d7cd
Artem <amarskiywork@gmail.com>
2024-02-23 00:56:25 +0300

    add list of two degrees func

--- FirstHomework/Program.fs
+++ FirstHomework/Program.fs
@@ -18,13 +18,16 @@ let reverse ls =
         else helper (List.tail donor) (List.head donor :: recipient)
     helper ls []
 
+let degreesOfTwo n m =
+    let rec helper ls degree number =
+        if degree = m + 1 then ls
+        else
+            helper (number :: ls) (degree + 1) (number * 2)
+    reverse (helper [] n (pown 2 n))
+
 let findNumber ls x =
     let rec helper ls n =
         if ls = [] then failwith "Not found"
         elif List.head ls = x then n
         else helper (List.tail ls) (n + 1)
     helper ls 0
-
-
-//let x = reverse [1 .. 10]
-printfn "%d" (findNumber [0 .. 10] 8)
