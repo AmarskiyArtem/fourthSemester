3eb8fd80536d79a43365351fb33779aabfbb7346
Artem <amarskiywork@gmail.com>
2024-03-30 01:03:36 +0300

    complete task

--- ThirdHomework/Program.fs
+++ ThirdHomework/Program.fs
@@ -11,12 +11,11 @@ let rec freeVars term =
 
 let isFree v term = Set.contains v (freeVars term)
 
-let rec freshVar usedVars =
-    let rec freshVar' i =
-        let v = "v" + i.ToString()
-        if Set.contains v usedVars then freshVar' (i + 1)
+let rec newVar usedVars =
+    let rec freshVar' v =
+        if Set.contains v usedVars then freshVar' v + "x"
         else v
-    freshVar' 0
+    freshVar' "x"
 
 let rec substitute x baseTerm term =
     match baseTerm with
@@ -27,13 +26,13 @@ let rec substitute x baseTerm term =
     | Abstraction (y, t) 
         when not(isFree y term && isFree x t) -> Abstraction (y, substitute x t term)
     | Abstraction (y, t) ->
-        let z = freshVar (Set.union (freeVars term) (freeVars t))
+        let z = newVar (Set.union (freeVars term) (freeVars t))
         Abstraction (z, substitute x (substitute y t (Var z)) term)
 
 
 let rec betaReduce term =
     match term with
+    | Var v -> Var v
     | Application (Abstraction (v, t), t') -> substitute v t' t
     | Application (t1, t2) -> Application (betaReduce t1, betaReduce t2)
     | Abstraction (v, t) -> Abstraction (v, betaReduce t)
-    | Var v -> Var v
