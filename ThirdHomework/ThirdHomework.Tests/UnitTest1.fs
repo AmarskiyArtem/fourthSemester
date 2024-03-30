5cbbb730de1d8d503732bc0dcef4b22bdfc47d33
Artem <amarskiywork@gmail.com>
2024-03-30 13:17:07 +0300

    add tests

--- ThirdHomework/ThirdHomework.Tests/UnitTest1.fs
+++ ThirdHomework/ThirdHomework.Tests/UnitTest1.fs
@@ -1,11 +1,31 @@
 module ThirdHomework.Tests
 
 open NUnit.Framework
+open FsUnit
+open BetaReduction
 
-[<SetUp>]
-let Setup () =
-    ()
+let Expressions () =
+    [
+        Application(
+            Abstraction("x", Var "y"),
+            Application(
+                Abstraction("x", Application(Var "x", Application(Var "x", Var "x"))),
+                Abstraction("x", Application(Var "x", Application(Var "x", Var "x")))
+            )
+        ), Var "y"
+        Application(Abstraction("x", Var "x"), Var "y"), Var "y"
+        Application(
+            Abstraction(
+                "f",
+                Abstraction(
+                    "x",
+                    Application(Application(Var "f", Var "x"), Var "x"))
+            ),
+            Var "+"
+        ), Abstraction("x", Application(Application(Var "+", Var "x"), Var "x"))
+    ]|> List.map (fun (term, normForm) -> TestCaseData(term, normForm))
 
-[<Test>]
-let Test1 () =
-    Assert.Pass()
+
+[<TestCaseSource(nameof Expressions)>]
+let betaReduceTests expression expected  =  
+    betaReduce expression |> should equal expected
